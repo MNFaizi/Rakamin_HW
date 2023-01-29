d20ffe0951479ee17cef9fabeddc7c750902fdf5
Faiz <mfaiz1365@gmail.com>
2023-01-29 10:51:24 +0700

    fixing

--- algoritmaAnalisisData.js
+++ algoritmaAnalisisData.js
@@ -5,7 +5,7 @@ function randomArray(min,max){
     const evenNumber =[];
     const oddNumber = [];
     for(i = min; i < max; i++){
-        let num = Math.floor(Math.random()*((max - min)+1));
+        let num = Math.floor(Math.random()*((max - min)+min));
         numbRand.push(num);
         if(num % 2 == 0){
             evenNumber.push(num);
@@ -15,11 +15,12 @@ function randomArray(min,max){
         }
     }
     console.log(numbRand);
-    console.log(oddNumber);
     console.log(evenNumber);
+    console.log(oddNumber);
     // nilai maximum dan minimum angka genap
-    let nilaiTerbesarEven = min;
-    let nilaiTerkecilEven = max;
+
+    let nilaiTerbesarEven = evenNumber[0];
+    let nilaiTerkecilEven = evenNumber[0];
     for(i = 0; i < evenNumber.length; i++){
         if(evenNumber[i] > nilaiTerbesarEven){
             nilaiTerbesarEven = evenNumber[i];
@@ -29,18 +30,21 @@ function randomArray(min,max){
         }
     }
     console.log(nilaiTerbesarEven,nilaiTerkecilEven);
+
     // nilai totalEven
-    let nilaiTotalEven = 0;
-    let nilaiRataEven = 0;
+    let nilaiTotalEven = evenNumber[0];
+    let nilaiRataEven = evenNumber[0];
     for(let i in evenNumber){
-        nilaiTotalEven += evenNumber[1];
+        nilaiTotalEven += evenNumber[i];
         nilaiRataEven = nilaiTotalEven/evenNumber.length;
     }
     console.log("Nilai Total Genap "+nilaiTotalEven);
     console.log("Nilai Rata-Rata Genap "+nilaiRataEven);
+    console.log("panjang array "+ evenNumber.length)
+
     // nilai maximum dan minimum angka ganjil
-    let nilaiTerbesarOdd = min;
-    let nilaiTerkecilOdd = max;
+    let nilaiTerbesarOdd = oddNumber[0];
+    let nilaiTerkecilOdd = oddNumber[0];
     for(i = 0; i < oddNumber.length; i++){
         if(oddNumber[i] > nilaiTerbesarOdd){
             nilaiTerbesarOdd = oddNumber[i];
@@ -50,15 +54,17 @@ function randomArray(min,max){
         }
     }
     console.log(nilaiTerbesarOdd, nilaiTerkecilOdd);
+
      // nilai totalOdd
-     let nilaiTotalOdd = 0;
-     let nilaiRataOdd = 0;
+     let nilaiTotalOdd = oddNumber[0];
+     let nilaiRataOdd = oddNumber[0];
      for(let i in oddNumber){
          nilaiTotalOdd += oddNumber[1];
          nilaiRataOdd = nilaiTotalOdd/oddNumber.length;
      }
      console.log("nilai Total Ganjil "+ nilaiTotalOdd);
      console.log("Nilai Rata-Rata Ganjil "+ nilaiRataOdd);
+     console.log("panjang array " + oddNumber.length)
     //  Perbandingan Rata-Rata
     if(nilaiRataEven>nilaiRataOdd){
         console.log("Nilai Rata genap lebih besar dari ganjil");       
@@ -87,10 +93,8 @@ function randomArray(min,max){
     else{
         console.log("nilai manimum ganjil lebih besar dari genap");
     }
-
-
 };
 
-randomArray(1,100);
+randomArray(1,101);
 
-myGithub("https://github.com/MNFaizi/Rakamin_HW/blob/master/algoritmaAnalisisData.js")
+// myGithub("https://github.com/MNFaizi/Rakamin_HW/blob/master/algoritmaAnalisisData.js")
