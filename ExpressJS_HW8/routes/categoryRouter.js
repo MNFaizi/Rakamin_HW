f557f157da14196e73edd22050ff39fd9a213a20
Mochamad NurFaizi <mfaiz1365@gmail.com>
2023-02-26 08:44:53 +0700

    add query select by category name

--- ExpressJS_HW8/routes/categoryRouter.js
+++ ExpressJS_HW8/routes/categoryRouter.js
@@ -12,9 +12,22 @@ router.get('/category', (req,resp) => {
         })
 })
 
-router.get('/category/:id', (req, resp) => {
-    const id = parseInt(req.params.id);
-    db.query('select * from category where category_id=$1', [id], (err, res) => {
+router.get('/category/:categoryname', (req, resp) => {
+    const categoryName = req.params.categoryname;
+    // const categoryQuery = `select 
+    //                             film.film_id as id,
+    //                             film.title as title,
+    //                             film.description as description,
+    //                             film.release_year as release_year,
+    //                             category.name as category
+    //                         from 
+    //                             film
+    //                             inner join film_category
+    //                                 on film.film_id = film_category.film_id
+    //                             inner join category
+    //                                 on category.category_id = film_category.film_id
+    //                             where film_category = $1`
+    db.query('select * from category where name = $1', [categoryName], (err, res) => {
         if(err){
             throw err
         }
